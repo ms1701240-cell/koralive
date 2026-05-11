9ec28375896a80edc8cdf905fa44dfb0ba72a068
ms1701240-cell <ms1701240@gmail.com>
2026-05-11 18:22:01 +0300

    added ads.txt and adx tags

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 // في ملف next.config.js أو middleware.ts
-const cspHeader = `default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.google.com https://*.googletagservices.com https://securepubads.g.doubleclick.net https://imasdk.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://*.google.com https://*.googlesyndication.com https://*.doubleclick.net https://images.unsplash.com; connect-src 'self' https://*.google.com https://*.doubleclick.net https://*.supabase.co https://*.googletagservices.com; frame-src 'self' https://*.google.com https://*.doubleclick.net https://*.googlesyndication.com https://*.ok.ru;`.replace(/\n/g, "");
+const cspHeader = `default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.google.com https://*.googletagservices.com https://*.googlesyndication.com https://*.doubleclick.net https://securepubads.g.doubleclick.net https://imasdk.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://*.google.com https://*.googlesyndication.com https://*.doubleclick.net https://images.unsplash.com; connect-src 'self' https://*.google.com https://*.doubleclick.net https://*.supabase.co https://*.googletagservices.com https://*.googlesyndication.com; frame-src 'self' https://*.google.com https://*.doubleclick.net https://*.googlesyndication.com https://*.ok.ru;`.replace(/\n/g, "");
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -14,19 +14,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader,
-          },
-        ],
-      },
-    ];
-  },
+  
 };
 
 export default nextConfig;

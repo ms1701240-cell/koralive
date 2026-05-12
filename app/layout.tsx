a34d5e48a3ef5de2cca0947d824e8f41328bbda2
ms1701240-cell <ms1701240@gmail.com>
2026-05-12 18:03:52 +0300

    added ads.txt and adx tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
               googletag.pubads().enableSingleRequest();
 
               // حجز المساحة عشان الموقع ما يتهزش (Layout Shift)
-              googletag.pubads().collapseEmptyDivs(false);
+              googletag.pubads().collapseEmptyDivs();
               
               googletag.enableServices();
             });
